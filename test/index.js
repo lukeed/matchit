2c0d3609a018f95f0fa6a71d7f142ebe02378e18
Luke Edwards <luke.edwards05@gmail.com>
2017-11-03 12:27:36 -0700

    add initial `parse` tests — with test helper

--- test/index.js
+++ test/index.js
@@ -3,6 +3,32 @@ const $ = require('../lib');
 
 const all = ['/', '/about', 'contact', '/books', '/books/*', '/books/:title'];
 
+function isEntry(t, segs, expect) {
+	t.true(Array.isArray(segs), '~> entry is an array of segments');
+	t.is(segs.length, expect.length, `~> entry has ${expect.length} segment(s)`)
+
+	segs.forEach((obj, idx) => {
+		t.deepEqual(Object.keys(obj), ['val', 'type'], '~~> segment has `val` & `type` keys');
+		t.is(typeof obj.type, 'number', '~~> segment.type is a number');
+		t.is(obj.type, expect[idx].type, '~~> segment.type returns expected value');
+		t.is(typeof obj.val, 'string', '~~> segment.val is a string');
+		t.is(obj.val, expect[idx].val, '~~> segment.val returns expected value');
+	});
+}
+
+function toParse(t, ins, outs) {
+	const res = $.parse(ins);
+
+	t.true(Array.isArray(res), 'returns an array');
+	t.is(res.length, ins.length, `returns ${ins.length} item(s)`);
+
+	res.forEach((val, idx) => {
+		isEntry(t, val, outs[idx]);
+	});
+
+	t.end();
+}
+
 test('matchit', t => {
 	t.is(typeof $, 'object', 'exports an object');
 	const keys = Object.keys($);
@@ -11,6 +37,19 @@ test('matchit', t => {
 		t.is(typeof $[k], 'function', `exports.${k} is a function`);
 	});
 	t.end();
+});
+
+test('parse empty', t => {
+	const out = $.parse([]);
+	t.true(Array.isArray(out), 'returns an array');
+	t.is(out.length, 0, 'returns an empty array');
+	t.end();
+});
+
+test('parse index', t => {
+	const input = ['/'];
+	const expect = [{ type:0, val:'/' }];
+	toParse(t, input, [expect]);
 });
 
 test('match index', t => {
