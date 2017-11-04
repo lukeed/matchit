7ae2db2b6258fd359e927271b2829070022b31b4
Luke Edwards <luke.edwards05@gmail.com>
2017-11-04 00:39:55 -0700

    update `match`  function;
    - strip & split the incoming string
    - match against existing `arr` definitions
    - filter down by num of segments first
    - prefer static > param > wildcard

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ function isEntry(t, segs, expect) {
 	t.is(segs.length, expect.length, `~> entry has ${expect.length} segment(s)`)
 
 	segs.forEach((obj, idx) => {
-		t.is(Object.keys(obj).length, 2, '~~> segment has `type` & `val` keys');
+		t.is(Object.keys(obj).length, 3, '~~> segment has `old`, `type` & `val` keys');
 		t.is(typeof obj.type, 'number', '~~> segment.type is a number');
 		t.is(obj.type, expect[idx].type, '~~> segment.type returns expected value');
 		t.is(typeof obj.val, 'string', '~~> segment.val is a string');
