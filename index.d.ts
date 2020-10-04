17df39f2a8db9a774772eef0896e76cf1c468b68
Luke Edwards <luke.edwards05@gmail.com>
2020-10-04 15:09:16 -0700

    chore: cleanup types

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,11 @@
-declare module "matchit" {
-  export function exec(
-    url: string,
-    match: Array<{ old: string; type: number; val: string }>
-  ): { [index: string]: string };
-  export function match(
-    path: string,
-    routes:
-      | Array<{ old: string; type: number; val: string }>
-      | Array<Array<{ old: string; type: number; val: string }>>
-  ): Array<{ old: string; type: number; val: string }>;
-  export function parse(path: string): Array<{ old: string; type: number; val: string }>;
+export interface Segment {
+	old: string;
+	type: number;
+	val: string;
 }
+
+export type Route = Segment[];
+
+export function exec(url: string, match: Route): Record<string, string>;
+export function match(path: string, routes: Route[]): Route;
+export function parse(path: string): Route;
